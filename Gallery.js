ed34fc461d349b49611bb37e4759f9cb29f74fb5
AvniJain30 <64777521+AvniJain30@users.noreply.github.com>
2024-07-23 18:19:41 +0530

    Add files via upload

---
+++ Gallery.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Gallery.css';
+
+const Gallery = ({ photos, onDeletePhoto }) => {
+  return (
+    <div className="gallery">
+      {photos.map(photo => (
+        <div className="photo-card" key={photo.id}>
+          <img src={photo.src} alt={photo.description} />
+          
+          <p>{photo.description}</p>
+          <button onClick={() => onDeletePhoto(photo.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
